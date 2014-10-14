a6de688a75cf9c8a42e71f0c4e889d529da5e006
thansen <thansen@solire.fr>
2014-10-14 15:55:23 +0200

    Adding Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+// Generated on 2014-08-28 using generator-webapp 0.4.9
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    uglify: {
+      options: {
+        preserveComments : 'some'
+      },
+      my_target: {
+        files: {
+          'dist/cookiebanner.min.js': ['src/cookiebanner.js'],
+          'dist/cookiebanner-accessibility.min.js': ['src/cookiebanner-accessibility.js']
+        }
+      }
+    },
+    copy: {
+      main: {
+        src: 'src/.htaccess',
+        dest: 'dist/.htaccess'
+      }
+    }
+  });
+
+  grunt.registerTask('build', ['uglify', 'copy']);
+};
